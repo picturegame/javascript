121fd69de171e10da92e6b6d7e51ba418b2b59cb
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-04-06 21:55:06 -0400

    filled out the Account component
    commented out the functions and index until I can confer tomorrow.

--- src/js/Account.js
+++ src/js/Account.js
@@ -1,1 +1,47 @@
-import React, {Component, PropType} from 'react';
+import React, {Component, PropTypes} from 'react';
+import SSF from 'react-simple-serial-form';
+
+export default class Account extends Component {
+	static propTypes = {
+		username: PropTypes.string.isRequired,
+		email: PropTypes.string.isRequired,
+		password: PropTypes.string.isRequired,
+		//onSave: PropTypes.func.isRequired
+	}
+
+	dataHandler(formData) {
+		data.file = this.file;
+		//this.props.onSave(formData);
+	}
+
+
+
+
+
+	render () {
+		return (
+			<div className="formView">
+				<SSF onData={::this.dataHandler}>
+					<div>
+						<label>
+						 Username
+						<input type="text" name="username"/>
+						</label>
+					</div>
+					<div>
+						<label>
+						 Email
+						<input type="text" name="email"/>
+						</label>
+					</div>
+					<div>
+						<label>
+						Password
+						<input type="hidden" name="password"/>
+						</label>
+					</div>
+				</SSF>
+			</div>
+			)
+	}
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
 // Javascript Entry Point
 import React from 'react';
 import Startscreen from './Startscreen';
+import Account from './Account';
 
 
-//let renderStart = () => ReactDOM.render(
+//let renderStart = () => 
+	//const url = '';
+
+
+
+	//ajax({url}).then(data => {
+		//ReactDOM.render(
 	//<Startscreen onCreate={renderSignIn}/>
 	//, document.querySelector('.app')
 	//);
+//};
+
+
+
+//ajax({
+	//url:'',
+	//type: 'POST'
+	//data: data,
+	//cache: false,
+	//dataType: 'json',
+	//processData: false,
+	//contentType: false
+//}).then(() => {
+	//renderStart();
+//});
