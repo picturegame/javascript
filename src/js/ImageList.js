3bc96621b6ecca3672ac1954d1a139889fa6d11a
JONATHAN SILVA <silva.jon86@gmail.com>
2016-04-08 11:07:49 -0400

    JD helped us with ImageList

--- src/js/ImageList.js
+++ src/js/ImageList.js
@@ -2,30 +2,33 @@ import React, {Component, PropTypes} from 'react';
 
 export default class ImageList extends Component {
 	static propTypes = {
-		user: PropTypes.arrayOf(PropTypes.shape ({
+		
+		images: PropTypes.arrayOf(PropTypes.shape ({
 			img_url: PropTypes.string.isRequired,
 			title: PropTypes.string.isRequired,
-			onImgSelect: PropTypes.func.isRequired
-			})).isRequired,
-			username: PropTypes.string.isRequired
+		})).isRequired,
+		
+		onImgSelect: PropTypes.func.isRequired,
+		
+		username: PropTypes.string.isRequired
 	}
 
-	getImage(user) {
-	let {onImgSelect, title, img_url} = this.props;
+	getImage(image) {
+	let {onImgSelect} = this.props;
 	return (
-		<div onClick={onImgSelect.bind(null, user)}>
-			<h5>{user.title}</h5>
-			<img className="img-list-unique" src={user.image_url}/>
+		<div onClick={onImgSelect.bind(null, image)}>
+			<h5>{image.title}</h5>
+			<img className="img-list-unique" src={image.img_url}/>
 		</div>
 		);
 	}
 
 	render () {
-		let { username, img_url} = this.props;
+		let { username, images} = this.props;
 		return(
 			<div className="img-list-wrapper">
 				<h4>{username}</h4>
-				<div>{user.map(::this.getImage)}</div>
+				<div>{images.map(::this.getImage)}</div>
 			</div>
 		);
 	}
