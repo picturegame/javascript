a4ca3d40e52346327b0a9aeb7f3e48ca195e00c4
JONATHAN SILVA <silva.jon86@gmail.com>
2016-04-08 08:37:45 -0400

    working on ImageList debug

--- src/js/ImageList.js
+++ src/js/ImageList.js
@@ -2,11 +2,9 @@ import React, {Component, PropTypes} from 'react';
 
 export default class ImageList extends Component {
 	static propTypes = {
-		user: PropTypes.arrayOf(PropTypes.shape ({
-			img_url: PropTypes.string.isRequired,
-			title: PropTypes.string.isRequired,
-			username: PropTypes.string.isRequired,
-		})).isRequired,
+		img_url: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		username: PropTypes.string.isRequired,
 		onImgSelect: PropTypes.func.isRequired
 	}
 
@@ -14,18 +12,18 @@ export default class ImageList extends Component {
 	let {onImgSelect, title, img_url} = this.props;
 	return (
 		<div onClick={onImgSelect.bind(null, user)}>
-			<h5>{user.title}</h5>
-			<img className="img-list-unique" src={user.image_url}/>
+			<h5>{title}</h5>
+			<img className="img-list-unique" src={image_url}/>
 		</div>
 		);
 	}
 
 	render () {
-		let {user, username} = this.props;
+		let {user, username, img_url} = this.props;
 		return(
 			<div className="img-list-wrapper">
-				<h4>{user.username}</h4>
-				<div>{user.map(::this.getImage)}</div>
+				<h4>{username}</h4>
+				<div>{img_url.map(::this.getImage)}</div>
 			</div>
 		);
 	}
